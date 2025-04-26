a5867c59e5e7b8ef8c4b7b67195cd4bcf4e705ff
wadsahin <wadsahinalom@gmail.com>
2025-04-25 17:55:34 -0700

    Display user name on mouse hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 
 const Navbar = () => {
   const {user, logOut} = useContext(AuthContext);
+  const [hover, setHover] = useState(false);
+  // On mouse hover
+  const onMouseHover = (e) =>{
+    e.preventDefault();
+    setHover(true);
+  }
+  // On mouse leave
+  const onMouseOut = (e) =>{
+    e.preventDefault();
+    setHover(false);
+  }
+
   // console.log(user?.photoURL);
   const userProfile = user?.photoURL; 
 
@@ -16,8 +28,15 @@ const Navbar = () => {
         <Link to="/about">About</Link>
       </div>
       <div className="login flex gap-2 items-center">
-        <div className="w-14 h-14">
-          <img className="rounded-full object-cover" src={userProfile } alt="" />
+        <div className="flex items-center gap-3">
+          {
+            hover && <strong className="">{user?.displayName}</strong>
+          }
+          <img 
+          className="w-14 h-14 rounded-full object-cover" 
+          onMouseEnter={(e) => onMouseHover(e)}
+          onMouseLeave={(e) => onMouseOut(e)}
+          src={userProfile } alt="" />
         </div>
         {user ? <button onClick={logOut} className="btn btn-error text-white">Logout</button>: <Link to="/auth/login" className="btn btn-neutral rounded-none">Login</Link>}
       </div>
